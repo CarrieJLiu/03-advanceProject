1d36896ec5c488579ef3c1ee21c7cc080d6f78b9
Carrie <carrielau316@gmail.com>
2023-03-22 12:29:23 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import List from "./tutorial/05-leverage-javascript/start/List";
+
+
+function App() {
+  return (
+    <div className='container'>
+      <h2>Advanced React</h2>
+      <List />
+    </div>
+  );
+}
+
+export default App;
